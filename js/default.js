c358e123bf1464d2314ba3c7c41bedb7dda972b9
macie <maciek.zok+github@gmail.com>
2014-11-27 19:18:42 +0100

    better structure

--- js/default.js
+++ js/default.js
@@ -6,16 +6,6 @@ var laser = (function() {
     var laserBeam = $('.laser_beam');
     var laserButton = $('#laser-button');
 
-    function init() {
-        var isLaserOn = true;
-        laserButton.parent()
-            .css('cursor', 'pointer')
-            .on('click', function() {
-                isLaserOn ? powerOff() : powerOn();
-                isLaserOn = !isLaserOn;
-            });
-    };
-
     function powerOn() {
         /***
         Turn on laser.
@@ -40,7 +30,17 @@ var laser = (function() {
         );
     };
 
-    return {init: init};
+    return {
+        init: function() {
+            var isLaserOn = true;
+            laserButton.parent()
+                .css('cursor', 'pointer')
+                .on('click', function() {
+                    isLaserOn ? powerOff() : powerOn();
+                    isLaserOn = !isLaserOn;
+                });
+        }
+    };
 })();
 
 
@@ -51,94 +51,93 @@ var frames = (function() {
     ***/
     var all_frames = $('.frames');
 
-    function hide() {
-        /***
-        Hide all frames and labels.
+    return {
+        hide: function() {
+            /***
+            Hide all frames and labels.
 
-        ***/
-        all_frames
-            .css({'opacity': '1'})
-            .find('.frame_border')
-                .css({'opacity': '0'})
-            .end()
-            .find('.frame_label')
-                .children()
+            ***/
+            all_frames
+                .css({'opacity': '1'})
+                .find('.frame_border')
                     .css({'opacity': '0'})
                 .end()
-            .end()
-            .find('.frame_label_name')
-                .children()
-                    .css({'opacity': '0'});
+                .find('.frame_label')
+                    .children()
+                        .css({'opacity': '0'})
+                    .end()
+                .end()
+                .find('.frame_label_name')
+                    .children()
+                        .css({'opacity': '0'});
+        },
+
+        show: function() {
+            /***
+            Show frames in specified sequence.
+
+            ***/
+            var showSequence = [
+                {
+                    elements: all_frames.find('.frame_border'),
+                    properties: 'transition.fadeIn',
+                    options: {duration: 1500, delay: 500}
+                },
+                {
+                    elements: all_frames.find('#frame_A-label'),
+                    properties: 'transition.bounceRightIn',
+                    options: {duration: 100}
+                },
+                {
+                    elements: all_frames.find('#frame_A-label_name'),
+                    properties: 'transition.fadeIn',
+                    options: {duration: 80}
+                },
+                {
+                    elements: all_frames.find('#frame_B-label'),
+                    properties: 'transition.bounceLeftIn',
+                    options: {duration: 100, delay: 200}
+                },
+                {
+                    elements: all_frames.find('#frame_B-label_name'),
+                    properties: 'transition.fadeIn',
+                    options: {duration: 80}
+                },
+                {
+                    elements: all_frames.find('#frame_C-label'),
+                    properties: 'transition.bounceLeftIn',
+                    options: {duration: 100, delay: 200}
+                },
+                {
+                    elements: all_frames.find('#frame_C-label_name'),
+                    properties: 'transition.fadeIn',
+                    options: {duration: 80}
+                },
+                {
+                    elements: all_frames.find('#frame_D-label'),
+                    properties: 'transition.bounceLeftIn',
+                    options: {duration: 100, delay: 200}
+                },
+                {
+                    elements: all_frames.find('#frame_D-label_name'),
+                    properties: 'transition.fadeIn',
+                    options: {duration: 80}
+                },
+                {
+                    elements: all_frames.find('#frame_E-label'),
+                    properties: 'transition.bounceUpIn',
+                    options: {duration: 100, delay: 200}
+                },
+                {
+                    elements: all_frames.find('#frame_E-label_name'),
+                    properties: 'transition.fadeIn',
+                    options: {duration: 80}
+                }
+            ];
+
+            $.Velocity.RunSequence(showSequence);
+        }
     };
-
-    function show() {
-        /***
-        Show frames in specified sequence.
-
-        ***/
-        var showSequence = [
-            {
-                elements: all_frames.find('.frame_border'),
-                properties: 'transition.fadeIn',
-                options: {duration: 1500, delay: 500}
-            },
-            {
-                elements: all_frames.find('#frame_A-label'),
-                properties: 'transition.bounceRightIn',
-                options: {duration: 100}
-            },
-            {
-                elements: all_frames.find('#frame_A-label_name'),
-                properties: 'transition.fadeIn',
-                options: {duration: 80}
-            },
-            {
-                elements: all_frames.find('#frame_B-label'),
-                properties: 'transition.bounceLeftIn',
-                options: {duration: 100, delay: 200}
-            },
-            {
-                elements: all_frames.find('#frame_B-label_name'),
-                properties: 'transition.fadeIn',
-                options: {duration: 80}
-            },
-            {
-                elements: all_frames.find('#frame_C-label'),
-                properties: 'transition.bounceLeftIn',
-                options: {duration: 100, delay: 200}
-            },
-            {
-                elements: all_frames.find('#frame_C-label_name'),
-                properties: 'transition.fadeIn',
-                options: {duration: 80}
-            },
-            {
-                elements: all_frames.find('#frame_D-label'),
-                properties: 'transition.bounceLeftIn',
-                options: {duration: 100, delay: 200}
-            },
-            {
-                elements: all_frames.find('#frame_D-label_name'),
-                properties: 'transition.fadeIn',
-                options: {duration: 80}
-            },
-            {
-                elements: all_frames.find('#frame_E-label'),
-                properties: 'transition.bounceUpIn',
-                options: {duration: 100, delay: 200}
-            },
-            {
-                elements: all_frames.find('#frame_E-label_name'),
-                properties: 'transition.fadeIn',
-                options: {duration: 80}
-            }
-        ];
-
-        $.Velocity.RunSequence(showSequence);
-    };
-
-    return {hide: hide,
-            show: show};
 })();
 
 
